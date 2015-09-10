a9418fe4bddcb35bad6928d9cd3b11ef6ee618cf
Nicolas Devaux <ndevaux@gmail.com>
2015-09-10 20:54:27 +0200

    update anim

--- _js/main.js
+++ _js/main.js
@@ -4,12 +4,15 @@
         nicodev = $('.trig'),
         tlAnim;
 
+
+
     tlAnim = new TimelineMax({repeat: -1, repeatDelay: .5});
 
     tlAnim
         .set([trois, nicodev], {autoAlpha: 0})
         .fromTo(nicodev, 2, {autoAlpha: 0, xPercent: '-1000'}, {autoAlpha: 1, xPercent: '0', ease: Power4.easeInOut})
         .add('nicodevIn')
-        .fromTo(trois, 3, {autoAlpha: 0, xPercent: '3000'}, {autoAlpha: 1, xPercent: '-48', ease: Power4.easeInOut});
+        .fromTo(trois, 3, {autoAlpha: 0, xPercent: '3000'}, {autoAlpha: 1, xPercent: '-48', ease: Power4.easeInOut})
+        .;
 
 })(jQuery);
