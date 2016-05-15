e8aa8bf83fbfef97a1cea91601bf0b9c988292db
nicodev3 <ndevaux@gmail.com>
2016-05-15 11:19:34 +0200

    update animations

--- _js/main.js
+++ _js/main.js
@@ -1,31 +1,34 @@
 (function ($) {
 
-    var n = $('.lettre-n'),
-        i = $('.lettre-i'),
-        c = $('.lettre-c'),
-        o = $('.lettre-o'),
-        l = $('.lettre-l'),
-        a = $('.lettre-a'),
-        s = $('.lettre-s'),
-        d = $('.lettre-d'),
-        e = $('.lettre-e'),
-        v = $('.lettre-v'),
-        a2 = $('.lettre-a2'),
-        u = $('.lettre-u'),
-        x = $('.lettre-x'),
-        trois = $('.trois'),
-        bat1 = $('#battery-1'),
-        bat2 = $('#battery-2'),
-        bat3 = $('#battery-3'),
-        bat4 = $('#battery-4'),
-        html = $('#html'),
-        css = $('#css'),
-        js = $('#js'),
+    var $n = $('.lettre-n'),
+        $i = $('.lettre-i'),
+        $c = $('.lettre-c'),
+        $o = $('.lettre-o'),
+        $l = $('.lettre-l'),
+        $a = $('.lettre-a'),
+        $s = $('.lettre-s'),
+        $d = $('.lettre-d'),
+        $e = $('.lettre-e'),
+        $v = $('.lettre-v'),
+        $a2 = $('.lettre-a2'),
+        $u = $('.lettre-u'),
+        $x = $('.lettre-x'),
+        $trois = $('.trois'),
+        $pileLevel = $('.pile-level'),
+        $tool = $('#tool-svg'),
+        $masoneryBlockOne = $('#masonery_block_1'),
+        $masoneryBlockTwo = $('#masonery_block_2'),
+        $masoneryBlockThree = $('#masonery_block_3'),
+        $masoneryBlockFour = $('#masonery_block_4'),
+
+        tlCompetences,
+        $html = $('#html'),
+        $css = $('#css'),
+        $js = $('#js'),
         tlAnim,
-        tlBattery,
         tlDomainName,
         time1,
-        domaine = $('.ndd-article'),
+        $domaine = $('.ndd-article'),
         $piedsRobot = $('#robot-foots'),
         $robot = $('#svg-robot'),
         $brasUn = $('#bras-1'),
@@ -35,7 +38,18 @@
         $responsive = $('#responsives'),
         tlSiteQuality;
 
-    tlSiteQuality = new TimelineMax({
+  tlCompetences = new TimelineMax({repeat: -1, repeatDelay: 4});
+
+  tlCompetences
+    .staggerFromTo($pileLevel, 0.5, {fill: "#fff"}, {fill: '#c8518d'}, 0.8)
+    .to($tool, .6, {scale: '0.5', ease: Bounce.easeIn}, '+=1')
+    .to($tool, .6, {scale: '1.7', ease: Bounce.easeOut})
+    .to($masoneryBlockThree, 0.3, {y: 51, ease: Power3.easeIn}, '+=1')
+    .to($masoneryBlockTwo, 0.6, {fill: 'none'})
+    .to($masoneryBlockFour, 0.3, {y: 51, ease: Power3.easeIn})
+    .to($masoneryBlockOne, 0.3, {x: 62, ease: Power3.easeIn});
+
+  tlSiteQuality = new TimelineMax({
       repeat: -1,
       yoyo: true
     });
@@ -66,37 +80,28 @@
     time1 = .3;
 
     tlAnim
-        .fromTo(l, time1, {autoAlpha: 1, y: 0}, {autoAlpha: 0, y: 20, ease: Power4.easeInOut})
-        .fromTo(a, time1, {autoAlpha: 1, y: 0}, {autoAlpha: 0, y: 20, ease: Power4.easeInOut})
-        .fromTo(s, time1, {autoAlpha: 1, y: 0}, {autoAlpha: 0, y: 20, ease: Power4.easeInOut})
-        .fromTo(a2, time1, {autoAlpha: 1, y: 0}, {autoAlpha: 0, y: 20, ease: Power4.easeInOut})
-        .fromTo(u, time1, {autoAlpha: 1, y: 0}, {autoAlpha: 0, y: 20, ease: Power4.easeInOut})
-        .fromTo(x, time1, {autoAlpha: 1, y: 0}, {autoAlpha: 0, y: 20, ease: Power4.easeInOut})
-        .fromTo(d,.2, {x: 0}, {x: -136, ease: Power4.easeInOut})
-        .fromTo(e,.2, {x: 0}, {x: -136, ease: Power4.easeInOut})
-        .fromTo(v,.2, {x: 0}, {x: -136, ease: Power4.easeInOut})
-        .fromTo(i,.2, {x: 0}, {x: -6, ease: Power4.easeInOut})
-        .fromTo(c,.2, {x: 0}, {x: -10, ease: Power4.easeInOut})
-        .fromTo(o,.2, {x: 0}, {x: -14, ease: Power4.easeInOut})
-        .to(d, .5, {x: -156, ease: Power4.easeInOut})
-        .to(e, .5, {x: -162, ease: Power4.easeInOut})
-        .to(v, .8, {x: -168, ease: Elastic.easeOut.config(1, 0.3)})
-        .to([n, i, c, o, d, e, v], .5, {backgroundColor: "#D3D39E", borderRadius: 0, ease: Linear.easeNone})
-        .to(trois, .3 , {autoAlpha: 1, ease: Power4.easeInOut })
-        .to(trois, 2, {autoAlpha: 1, x: -270, rotationY: 380, ease: Power4.easeInOut });
-
-    tlBattery = new TimelineMax({repeat: -1, repeatDelay: 6, yoyo: false, delay: 1});
-
-    tlBattery
-        .to(bat1,.3, {autoAlpha: 1,  ease: Power4.easeInOut})
-        .to(bat2,.3, {autoAlpha: 1,  ease: Power4.easeInOut})
-        .to(bat3,.3, {autoAlpha: 1,  ease: Power4.easeInOut})
-        .to(bat4,.3, {autoAlpha: 1,  ease: Power4.easeInOut})
-    ;
+        .fromTo($l, time1, {autoAlpha: 1, y: 0}, {autoAlpha: 0, y: 20, ease: Power4.easeInOut})
+        .fromTo($a, time1, {autoAlpha: 1, y: 0}, {autoAlpha: 0, y: 20, ease: Power4.easeInOut})
+        .fromTo($s, time1, {autoAlpha: 1, y: 0}, {autoAlpha: 0, y: 20, ease: Power4.easeInOut})
+        .fromTo($a2, time1, {autoAlpha: 1, y: 0}, {autoAlpha: 0, y: 20, ease: Power4.easeInOut})
+        .fromTo($u, time1, {autoAlpha: 1, y: 0}, {autoAlpha: 0, y: 20, ease: Power4.easeInOut})
+        .fromTo($x, time1, {autoAlpha: 1, y: 0}, {autoAlpha: 0, y: 20, ease: Power4.easeInOut})
+        .fromTo($d,.2, {x: 0}, {x: -136, ease: Power4.easeInOut})
+        .fromTo($e,.2, {x: 0}, {x: -136, ease: Power4.easeInOut})
+        .fromTo($v,.2, {x: 0}, {x: -136, ease: Power4.easeInOut})
+        .fromTo($i,.2, {x: 0}, {x: -6, ease: Power4.easeInOut})
+        .fromTo($c,.2, {x: 0}, {x: -10, ease: Power4.easeInOut})
+        .fromTo($o,.2, {x: 0}, {x: -14, ease: Power4.easeInOut})
+        .to($d, .5, {x: -156, ease: Power4.easeInOut})
+        .to($e, .5, {x: -162, ease: Power4.easeInOut})
+        .to($v, .8, {x: -168, ease: Elastic.easeOut.config(1, 0.3)})
+        .to([$n, $i, $c, $o, $d, $e, $v], .5, {backgroundColor: "#D3D39E", borderRadius: 0, ease: Linear.easeNone})
+        .to($trois, .3 , {autoAlpha: 1, ease: Power4.easeInOut })
+        .to($trois, 2, {autoAlpha: 1, x: -270, rotationY: 380, ease: Power4.easeInOut });
 
     tlDomainName = new TimelineMax({repeat:-1, repeatDelay: 4, yoyo: true, delay: 4});
     tlDomainName
-        .to(domaine, 2, {
+        .to($domaine, 2, {
         text:{
             value:"www.mon-site.fr",
             newClass:"ndd-article-pink",
@@ -113,8 +118,8 @@
 
     $('#animation-play').on('click', function (e) {
         tlServeur
-        .fromTo([html, css, js], 4, {x: 0, y: 0}, {x: 260, y: -120, ease: Power4.easeInOut})
-        .fromTo([html, css, js], 4, {x: 260, y: -120}, {x: 520, y: 0, ease: Power4.easeInOut})
+        .fromTo([$html, $css, $js], 4, {x: 0, y: 0}, {x: 260, y: -120, ease: Power4.easeInOut})
+        .fromTo([$html, $css, $js], 4, {x: 260, y: -120}, {x: 520, y: 0, ease: Power4.easeInOut})
     });
     
 })(jQuery);
