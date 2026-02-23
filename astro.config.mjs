e91fb8952e3680f2931a05f73714ffacf048bc47
Nicolas DEVAUX <ndevaux@gmail.com>
2026-02-23 20:23:38 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nicodev-astro.netlify.app',
+    site: 'https://nicodev.fr',
     vite: {
         plugins: [tailwindcss()]
     }
