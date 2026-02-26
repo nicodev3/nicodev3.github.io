7f11b10995e6b5fcdff0ff56746179f5711142f4
Nicolas DEVAUX <ndevaux@gmail.com>
2026-02-26 21:56:27 +0100

    rm GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
 
-import partytown from '@astrojs/partytown';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nicodev.fr',
@@ -16,8 +14,5 @@ export default defineConfig({
       plugins: [tailwindcss()]
   },
 
-  integrations: [sitemap(), partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }})]
+  integrations: [sitemap()]
 });
