673e62e9f3f883f2ba37baead86ecd30ef6232b6
Nicolas DEVAUX <ndevaux@gmail.com>
2026-05-23 14:39:24 +0200

    .

---
+++ src/data/contact.ts
@@ -0,0 +1,6 @@
+/** Numéro sans espaces ni + (wa.me, schémas structurés). */
+export const WHATSAPP_PHONE_E164 = '33629341367';
+
+export const WHATSAPP_HREF = `https://wa.me/${WHATSAPP_PHONE_E164}`;
+
+export const WHATSAPP_DISPLAY = '06 29 34 13 67';
